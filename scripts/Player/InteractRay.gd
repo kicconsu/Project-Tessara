e8b2bccb15edf938d59a6adfb68ff20cca08568d
Bokter <rojasdjuan18@gmail.com>
2024-06-04 20:52:39 -0500

    a
    a

--- scripts/Player/InteractRay.gd
+++ scripts/Player/InteractRay.gd
@@ -13,9 +13,12 @@ func _process(_delta):
 	prompt.text = ""
 	if is_colliding():
 		
+		print("aa")
 		var detected = get_collider()
 		
-		if detected is Interactable:
+		print(detected)
+		
+		if detected is Trigger:
 			prompt.text = detected.get_prompt()
 			
 			if Input.is_action_just_pressed(detected.prompt_action):
