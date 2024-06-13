19c940a7c5be2ba47bb5430a34a87c59f2d3cf72
Teffx410 <xxjuanmanderxx@gmail.com>
2024-06-12 19:08:52 -0500

    Musiquita en el menu

--- Scripts/Menus/level_selector.gd
+++ Scripts/Menus/level_selector.gd
@@ -10,8 +10,8 @@ func _on_d_level_pressed():
 func _on_d_level_1_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/4D Level.tscn")
 
-func _on_d_level_2_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/4DLev1.tscn")
-
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
+
+func _on_d_level_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/4DLev1.tscn")
